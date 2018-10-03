72b5189bde138614b2f805a3750cc218786e5870
Maurizio M <mau2603@aol.com>
2018-10-03 14:45:12 +0200

    Drag & Drop

---
+++ Sitzordnung_IT_Klassen/Sitzordnung_IT_Klassen/SchuelerGUI.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sitzordnung_IT_Klassen
+{
+    /// <summary>
+    /// Interaktionslogik für SchuelerGUI.xaml
+    /// </summary>
+    public partial class SchuelerGUI : UserControl
+    {
+        private Brush _previousFill = null;
+
+        public SchuelerGUI()
+        {
+            InitializeComponent();
+        }
+
+        public SchuelerGUI(SchuelerGUI s)
+        {
+            InitializeComponent();
+            this.schuelerUI.Height = s.schuelerUI.Height;
+            this.schuelerUI.Width = s.schuelerUI.Height;
+            this.schuelerUI.Fill = s.schuelerUI.Fill;
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                // Package the data.
+                DataObject data = new DataObject();
+                data.SetData(DataFormats.StringFormat, schuelerUI.Fill.ToString());
+                data.SetData("Double", schuelerUI.Height);
+                data.SetData("Object", this);
+
+                // Inititate the drag-and-drop operation.
+                DragDrop.DoDragDrop(this, data, DragDropEffects.Copy | DragDropEffects.Move);
+            }
+        }
+
+        protected override void OnGiveFeedback(GiveFeedbackEventArgs e)
+        {
+            base.OnGiveFeedback(e);
+            // These Effects values are set in the drop target's
+            // DragOver event handler.
+            if (e.Effects.HasFlag(DragDropEffects.Copy))
+            {
+                Mouse.SetCursor(Cursors.Cross);
+            }
+            else if (e.Effects.HasFlag(DragDropEffects.Move))
+            {
+                Mouse.SetCursor(Cursors.Pen);
+            }
+            else
+            {
+                Mouse.SetCursor(Cursors.No);
+            }
+            e.Handled = true;
+        }
+
+        protected override void OnDrop(DragEventArgs e)
+        {
+            base.OnDrop(e);
+
+            // If the DataObject contains string data, extract it.
+            if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
+
+                // If the string can be converted into a Brush, 
+                // convert it and apply it to the ellipse.
+                BrushConverter converter = new BrushConverter();
+                if (converter.IsValid(dataString))
+                {
+                    Brush newFill = (Brush)converter.ConvertFromString(dataString);
+                    schuelerUI.Fill = newFill;
+
+                    // Set Effects to notify the drag source what effect
+                    // the drag-and-drop operation had.
+                    // (Copy if CTRL is pressed; otherwise, move.)
+                    if (e.KeyStates.HasFlag(DragDropKeyStates.ControlKey))
+                    {
+                        e.Effects = DragDropEffects.Copy;
+                    }
+                    else
+                    {
+                        e.Effects = DragDropEffects.Move;
+                    }
+                }
+            }
+            e.Handled = true;
+        }
+
+        protected override void OnDragOver(DragEventArgs e)
+        {
+            base.OnDragOver(e);
+            e.Effects = DragDropEffects.None;
+
+            // If the DataObject contains string data, extract it.
+            if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
+
+                // If the string can be converted into a Brush, allow copying or moving.
+                BrushConverter converter = new BrushConverter();
+                if (converter.IsValid(dataString))
+                {
+                    // Set Effects to notify the drag source what effect
+                    // the drag-and-drop operation will have. These values are 
+                    // used by the drag source's GiveFeedback event handler.
+                    // (Copy if CTRL is pressed; otherwise, move.)
+                    if (e.KeyStates.HasFlag(DragDropKeyStates.ControlKey))
+                    {
+                        e.Effects = DragDropEffects.Copy;
+                    }
+                    else
+                    {
+                        e.Effects = DragDropEffects.Move;
+                    }
+                }
+            }
+            e.Handled = true;
+        }
+
+        protected override void OnDragEnter(DragEventArgs e)
+        {
+            base.OnDragEnter(e);
+            // Save the current Fill brush so that you can revert back to this value in DragLeave.
+            _previousFill = schuelerUI.Fill;
+
+            // If the DataObject contains string data, extract it.
+            if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
+
+                // If the string can be converted into a Brush, convert it.
+                BrushConverter converter = new BrushConverter();
+                if (converter.IsValid(dataString))
+                {
+                    Brush newFill = (Brush)converter.ConvertFromString(dataString.ToString());
+                    schuelerUI.Fill = newFill;
+                }
+            }
+        }
+
+    }
+}
